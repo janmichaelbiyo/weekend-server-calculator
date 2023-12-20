923282840848fbe5ef054cb0b6797dbcd98a864e
Jan Michael Biyo <janmichael.biyo@gmail.com>
2023-12-20 06:50:42 -0800

    got the get response to the client side by the server it poplulates the test array that i have

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,26 @@
-console.log('client.js is sourced!');
+function calculationTime(){
+    console.log('client.js is sourced!');
+
+    axios({
+        method: 'GET',
+        url:'/calculations'
+    })
+        .then(function(response) {
+            console.log(response.data);
+            let calculationServer = response.data;
+            let calculationDisplay = document.querySelector('#resultHistory');  
+            for (let calculation of calculationServer) {
+                calculationDisplay.innerHTML += `
+                <ul>
+                    <li> ${calculation.numOne} ${calculation.operator} ${calculation.numTwo} = ${calculation.result} </li>
+                </ul>
+                `; } 
+         }) .catch(function(error){
+            console.log(error)
+            alert('Something happened whats going on')
+         });
+}
+
+calculationTime();
+
+
